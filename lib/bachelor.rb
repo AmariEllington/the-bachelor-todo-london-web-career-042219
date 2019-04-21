7668195885313674aa0395004d2ed53c741d834f
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-21 21:07:05 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -41,7 +41,14 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |seaon, array|
+    array.each do |hash|
+      hash.each do |key, value|
+        if value == hometown
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
