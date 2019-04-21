d8f8220e3219163b2bdb250d7e7d58bb15b0106f
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-21 20:32:38 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,8 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season.to_sym] do |array|
+    if array[:status] == "Winner"
+      return array[:name].split(" ")[0]
+  
 end
 
 def get_contestant_name(data, occupation)
